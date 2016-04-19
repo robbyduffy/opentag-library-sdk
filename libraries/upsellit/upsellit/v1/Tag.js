2c9221b1748d1a0672b66eb00731d7e3f606b894
Mark Holmes <mholmes@upsellit.com>
2016-04-19 10:35:18 -0700

    Initial wizard code

---
+++ libraries/upsellit/upsellit/v1/Tag.js
@@ -0,0 +1,32 @@
+//:include tagsdk-current.js
+
+qubit.opentag.LibraryTag.define("upsellit.upsellit.v1.Tag", {
+	config: {
+		/*DATA*/
+		name: "upsellit",
+		async: false,
+		description: "",
+		html: "",
+		locationDetail: "",
+		isPrivate: false,
+		url: "",
+		usesDocWrite: false,
+		upgradeable: true,
+		parameters: [
+
+	]
+		/*~DATA*/
+	},
+	script: function() {
+	/*SCRIPT*/
+	/*~SCRIPT*/
+	},
+	pre: function() {
+	/*PRE*/
+	/*~PRE*/
+	},
+	post: function() {
+	/*POST*/
+	/*~POST*/
+	}
+});
