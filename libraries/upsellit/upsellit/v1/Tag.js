95318c596722d2d7243bf9a133e343d67cdf3add
Mark Holmes <mholmes@upsellit.com>
2016-04-27 09:42:28 -0700

    added new tag 'upsellit'

--- libraries/upsellit/upsellit/v1/Tag.js
+++ libraries/upsellit/upsellit/v1/Tag.js
@@ -3,30 +3,35 @@
 qubit.opentag.LibraryTag.define("upsellit.upsellit.v1.Tag", {
 	config: {
 		/*DATA*/
-		name: "upsellit",
-		async: false,
-		description: "",
-		html: "",
+		name: "Upsellit",
+		async: true,
+		description: ".",
+		html: "<!-- Start of Upsellit Code -->\n \n<!-- End of Upsellit Code -->",
 		locationDetail: "",
 		isPrivate: false,
 		url: "",
-		usesDocWrite: false,
+		usesDocWrite: true,
 		upgradeable: true,
-		parameters: [
-
-	]
+		parameters: [{
+			token: "launch_tag"
+		}]
 		/*~DATA*/
 	},
 	script: function() {
-	/*SCRIPT*/
-	/*~SCRIPT*/
+		/*SCRIPT*/
+			var USI_headID = document.getElementsByTagName("head")[0];
+			var USI_dynScript = document.createElement("script");
+			USI_dynScript.setAttribute('type','text/javascript');
+			USI_dynScript.src = '//www.upsellit.com/active/' + this.valueForToken("launch_tag") + '.jsp';
+			USI_headID.appendChild(USI_dynScript);
+		/*~SCRIPT*/
 	},
 	pre: function() {
-	/*PRE*/
-	/*~PRE*/
+		/*PRE*/
+		/*~PRE*/
 	},
 	post: function() {
-	/*POST*/
-	/*~POST*/
+		/*POST*/
+		/*~POST*/
 	}
 });
---
+++ libraries/upsellit/upsellit/v1/local/Config.js
@@ -0,0 +1,11 @@
+/**ignore at merge**/
+//:include tagsdk-current.js
+qubit.opentag.Utils.namespace('upsellit.upsellit.v1.local');
+upsellit.upsellit.v1.local.Config = {
+  "parameters": [
+     {
+      "token": "launch_tag",
+      "inputVariable": ""
+    }
+  ]
+};
