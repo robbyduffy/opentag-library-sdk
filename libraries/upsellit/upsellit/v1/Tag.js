4e8a934afce68958c8eba7663df6c2311c88f775
Peter Fronc <peter.fronc@qubit.com>
2016-05-24 11:17:13 +0100

    Merge master branch.

--- libraries/upsellit/upsellit/v1/Tag.js
+++ libraries/upsellit/upsellit/v1/Tag.js
@@ -1,39 +1,42 @@
 //:include tagsdk-current.js
 
 qubit.opentag.LibraryTag.define("upsellit.upsellit.v1.Tag", {
-	config: {
-		/*DATA*/
-		name: "Upsellit",
-		async: true,
-		description: ".",
-		html: "",
-		locationDetail: "",
-		isPrivate: false,
-		url: "",
-		usesDocWrite: true,
-		upgradeable: true,
-		parameters: [{
-			name: "Launch Tag",
-                        description: "",
-			token: "launch_tag"
-		}]
-		/*~DATA*/
+	getDefaultConfig: function () {
+    return {
+      /*config*/
+      name: "Upsellit",
+      async: true,
+      description: ".",
+      html: "",
+      locationDetail: "",
+      isPrivate: false,
+      url: "",
+      usesDocWrite: true,
+      upgradeable: true,
+      parameters: [{
+        name: "Launch Tag",
+        description: "",
+        token: "launch_tag"
+      }]
+      /*~config*/
+    };
 	},
 	script: function() {
-		/*SCRIPT*/
+		/*script*/
 			var USI_headID = document.getElementsByTagName("head")[0];
 			var USI_dynScript = document.createElement("script");
 			USI_dynScript.setAttribute('type','text/javascript');
-			USI_dynScript.src = '//www.upsellit.com/active/' + this.valueForToken("launch_tag") + '.jsp';
+			USI_dynScript.src = '//www.upsellit.com/active/' + 
+              this.valueForToken("launch_tag") + '.jsp';
 			USI_headID.appendChild(USI_dynScript);
-		/*~SCRIPT*/
+		/*~script*/
 	},
 	pre: function() {
-		/*PRE*/
-		/*~PRE*/
+		/*pre*/
+		/*~pre*/
 	},
 	post: function() {
-		/*POST*/
-		/*~POST*/
+		/*post*/
+		/*~post*/
 	}
 });
