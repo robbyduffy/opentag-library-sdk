565ee392f986e3e85ae84f0bbc1a537c4c1f33b9
Peter Fronc <peter.fronc@qubit.com>
2016-05-24 11:32:17 +0100

    Updating image link to a working one.

--- libraries/upsellit/Config.js
+++ libraries/upsellit/Config.js
@@ -2,7 +2,7 @@ var Config = {
 /*config*/
   "id": 1496153169315840,
   "name": "UpSellit",
-  "imageUrl": "https://www2.upsellit.com/chatskins/upsellit-arrow.png",
+  "imageUrl": "http://cdn.blog.cpcstrategy.com/wp-content/uploads/2014/02/UpSellit-site-abandonment-Logo.png",
   "description": "UpSellit increases online profitability with a suite of on-site and email solutions that deliver personalized conversion experiences. After an in-depth visitor behavior analysis, UpSellit designs completely custom conversion strategies that address specific pain points in the buying process."
 /*~config*/
 };
