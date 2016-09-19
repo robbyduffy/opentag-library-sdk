02904a208b814246c83f61e4e7dac998b4cef99a
Peter Fronc <peter.fronc@qubit.com>
2016-09-19 16:15:54 +0100

    Adding ping switch library.

---
+++ libraries/qubit/opentagrtsswitch/v1/Tag.js
@@ -0,0 +1,36 @@
+//:import CurrentSDK
+
+qubit.opentag.LibraryTag.define("qubit.opentagrtsswitch.v1.Tag", {
+	getDefaultConfig: function () {
+		return {
+			/*config*/
+		name: "Opentag RTS Switch",
+		async: false,
+		description: "",
+		html: "",
+		locationDetail: "",
+		isPrivate: false,
+		url: "",
+		usesDocWrite: false,
+		upgradeable: true,
+		parameters: [
+
+	]
+			/*~config*/
+		};
+	},
+	script: function() {
+	/*script*/
+		qubit.opentag.Ping.useNewPingSource = true;
+		qubit.opentag.Ping.pingServerUrl = "130.211.33.3/ping";
+	/*~script*/
+	},
+	pre: function() {
+	/*pre*/
+	/*~pre*/
+	},
+	post: function() {
+	/*post*/
+	/*~post*/
+	}
+});
