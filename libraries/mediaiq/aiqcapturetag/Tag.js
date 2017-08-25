9fc8eb4ce1cac40fa2060212b4fce7cc63391155
robbyduffy <31311901+robbyduffy@users.noreply.github.com>
2017-08-25 12:17:11 +0100

    Update Tag.js

--- libraries/mediaiq/aiqcapturetag/Tag.js
+++ libraries/mediaiq/aiqcapturetag/Tag.js
@@ -7,22 +7,146 @@ qubit.opentag.LibraryTag.define("mediaiq.aiqcapturetag.Tag", {
 		name: "AiQ data Capture Tag",
 		async: true,
 		description: "Our clients are AiQ enabled to receive DMP-type functionality, personnel support and hepl their campaigns perform.",
-		html: "<img src="https://secure.adnxs.com/px?id=${pixel_id}&seg=${segment_id)]&redir=https%3A%2F%2Fsecure.adnxs.com%2Fgetuid%3Fhttps%3A%2F%2Fpixel.mediaiqdigital.com%2Fpixel%3Fu1%3D${u1}%26u2%3D${u2}%26u3%3D${u3}%26u4%3D${u4}%26u5%3D${u5}%26u6%3D${u6}%26u7%3D${u7}%26u8%3D${u8}%26u9%3D${u9}%26u10%3D${u10}%26u11%3D${u11}%26u12%3D${u12}%26u13%3D${u13}%26u14%3D${u14}%26u15%3D${u15}%26u16%3D${u16}%26u17%3D${u17}%26u18%3D${u18}%26u19%3D${u19}%26u20%3D${u20}%26u21%3D${u21}%26u22%3D${u22}%26u23%3D${u23}%26u24%3D${u24}%26u25%3D${u25}%26pixel_id%3D${pixel_id}%26uid%3D%24UID&t=2" width="1" height="1" />
-",
+		html: "<img src=\"https://secure.adnxs.com/px?id=${pixel_id}&seg=${segment_id)]&redir=https%3A%2F%2Fsecure.adnxs.com%2Fgetuid%3Fhttps%3A%2F%2Fpixel.mediaiqdigital.com%2Fpixel%3Fu1%3D${u1}%26u2%3D${u2}%26u3%3D${u3}%26u4%3D${u4}%26u5%3D${u5}%26u6%3D${u6}%26u7%3D${u7}%26u8%3D${u8}%26u9%3D${u9}%26u10%3D${u10}%26u11%3D${u11}%26u12%3D${u12}%26u13%3D${u13}%26u14%3D${u14}%26u15%3D${u15}%26u16%3D${u16}%26u17%3D${u17}%26u18%3D${u18}%26u19%3D${u19}%26u20%3D${u20}%26u21%3D${u21}%26u22%3D${u22}%26u23%3D${u23}%26u24%3D${u24}%26u25%3D${u25}%26pixel_id%3D${pixel_id}%26uid%3D%24UID&t=2\" width=\"1\" height=\"1\" />",
 		locationDetail: "",
 		isPrivate: false,
 		url: "",
 		usesDocWrite: false,
 		upgradeable: true,
 		parameters: [{
-			name: "Media iQ",
-			description: "Our clients are AiQ enabled to receive DMP-type functionality, personnel support and hepl their campaigns perform.",
-			token: "media_iq",
+			name: "conversion pixel ID",
+			description: "The appnexus pixel to be requested for this tag fire",
+			token: "pixel_id",
 			uv: ""
 		}, {
-			name: "Media iQ",
-			description: "Our clients are AiQ enabled to receive DMP-type functionality, personnel support and hepl their campaigns perform.",
-			token: "media_iq",
+	      name: "segment pixel ID",
+			description: "OPTIONAL: The appnexus segment to be registered against for this tag fire",
+			token: "segment_id",
+			uv: ""
+	      		}, {
+	      name: "u-value 1",
+			description: "As decided with your Media iQ contact",
+			token: "u1",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 2",
+			description: "As decided with your Media iQ contact",
+			token: "u2",
+			uv: ""      	      		
+      }, {
+	      name: "u-value 3",
+			description: "As decided with your Media iQ contact",
+			token: "u3",
+			uv: ""      	      		
+      }, {
+	      name: "u-value 4",
+			description: "As decided with your Media iQ contact",
+			token: "u4",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 5",
+			description: "As decided with your Media iQ contact",
+			token: "u5",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 6",
+			description: "As decided with your Media iQ contact",
+			token: "u6",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 7",
+			description: "As decided with your Media iQ contact",
+			token: "u7",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 8",
+			description: "As decided with your Media iQ contact",
+			token: "u8",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 9",
+			description: "As decided with your Media iQ contact",
+			token: "u9",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 10",
+			description: "As decided with your Media iQ contact",
+			token: "u10",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 11",
+			description: "As decided with your Media iQ contact",
+			token: "u11",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 12",
+			description: "As decided with your Media iQ contact",
+			token: "u12",
+			uv: ""
+	      	      		}, {
+	      name: "u-value  13",
+			description: "As decided with your Media iQ contact",
+			token: "u13",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 14",
+			description: "As decided with your Media iQ contact",
+			token: "u14",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 15",
+			description: "As decided with your Media iQ contact",
+			token: "u15",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 16",
+			description: "As decided with your Media iQ contact",
+			token: "u16",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 17",
+			description: "As decided with your Media iQ contact",
+			token: "u7",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 18",
+			description: "As decided with your Media iQ contact",
+			token: "u18",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 19",
+			description: "As decided with your Media iQ contact",
+			token: "u19",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 20",
+			description: "As decided with your Media iQ contact",
+			token: "u20",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 21",
+			description: "As decided with your Media iQ contact",
+			token: "u21",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 22",
+			description: "As decided with your Media iQ contact",
+			token: "u22",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 23",
+			description: "As decided with your Media iQ contact",
+			token: "u23",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 24",
+			description: "As decided with your Media iQ contact",
+			token: "u24",
+			uv: ""
+	      	      		}, {
+	      name: "u-value 25",
+			description: "As decided with your Media iQ contact",
+			token: "u25",
 			uv: ""
 		}],
 		categories:[
